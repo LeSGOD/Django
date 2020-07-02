268a87aa9a3db392549ebf34d0cc26d9fd265995
Artur Leszczynski <artles00@wp.pl>
2020-07-02 23:38:51 +0200

    django_level_one

--- Django Level One/first_project/first_app/models.py
+++ Django Level One/first_project/first_app/models.py
@@ -1,23 +1,34 @@
 from django.db import models
 
-# Create your models here.
+# SuperUserInformation
+# User: Jose
+# Email: training@pieriandata.com
+# Password: testpassword
 
+# CREATE SOME TEST DATA WITH SOME SHELL COMMANDS:
+
+# python manage.py shell
+
+# from first_app.models import Topic
+# print(Topic.objects.all())
+# t = Topic(top_name="Social Network")
+# t.save()
+# print(Topic.objects.all())
+# quit()
 
 class Topic(models.Model):
-    top_name = models.CharField(max_length=264, unique=True)
+    top_name = models.CharField(max_length=264,unique=True)
 
     def __str__(self):
         return self.top_name
 
-
 class Webpage(models.Model):
-    category = models.ForeignKey(Topic)
-    name = models.CharField(max_length=264)
+    topic = models.ForeignKey(Topic)
+    name = models.CharField(max_length=264,unique=True)
     url = models.URLField(unique=True)
 
     def __str__(self):
         return self.name
-
 
 class AccessRecord(models.Model):
     name = models.ForeignKey(Webpage)
