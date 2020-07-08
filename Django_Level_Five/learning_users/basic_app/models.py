aeb2ed801cc308ea09a5b3220a51f6f3da4ae717
LeSGOD <artles00@wp.pl>
2020-07-08 22:30:36 +0200

    users

--- Django_Level_Five/learning_users/basic_app/models.py
+++ Django_Level_Five/learning_users/basic_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+def UserProfileInfo(models.Model):
+    user = models.OneToOneField(User)
+
+    portfolio = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_pics')
+
+    def __str__(self):
+        return self.user.username
