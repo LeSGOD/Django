00b51f379a995b3d1187b7a3da71881551a9f920
LeSGOD <artles00@wp.pl>
2020-07-09 20:15:58 +0200

    password

--- Django_Level_Five/learning_users/basic_app/models.py
+++ Django_Level_Five/learning_users/basic_app/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-def UserProfileInfo(models.Model):
+class UserProfileInfo(models.Model):
     user = models.OneToOneField(User)
 
-    portfolio = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_pics')
+    portfolio_site = models.URLField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics', blank=True)
 
     def __str__(self):
         return self.user.username
