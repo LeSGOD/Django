931ad7d7686e8a5dc3feb3bef4e200823cdffabc
Artur Leszczynski <artles00@wp.pl>
2020-07-19 16:53:53 +0200

    blog

--- blog_project/mysite/blog/urls.py
+++ blog_project/mysite/blog/urls.py
@@ -14,4 +14,12 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/remove/$',
         views.PostDeleteView.as_view(), name='post_remove'),
     url(r'^drafts/$', views.DraftListView.as_view(), name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/comment/$',
+        views.add_comment_to_post.as_view(), name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$',
+        views.comment_approve.as_view(), name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$',
+        views.comment_remove.as_view(), name='comment_remove'),
+    url(r'^comment/(?P<pk>\d+)/publish/$',
+        views.post_publish.as_view(), name='post_publish'),
 ]
